5c047d6edcc1cfdd0df44cf091a845b6123dcfeb
Zeynab <Nature4explorer@gmail.com>
2023-04-26 07:34:59 +0430

    Set the API info

--- src/App.js
+++ src/App.js
@@ -1,35 +1,77 @@
+import React, { useState } from "react";
 import "./App.css";
-import Form from "./Form";
-
+import axios from "axios";
 export default function App() {
+  let [city, setCity] = useState("");
+  let [showCity, setShowCity] = useState("");
+  let [showTemp, setShowTemp] = useState("");
+  let [showHumidity, setShowHumidity] = useState("");
+  let [showWindSpeed, setShowWindSpeed] = useState("");
+  let [showDescription, setShowDescription] = useState("");
+  let [showIcon, setShowIcon] = useState("");
+
+  function showTemperature(response) {
+    console.log(response);
+    setShowCity(response.data.name);
+    setShowTemp(response.data.main.temp);
+    setShowHumidity(response.data.main.humidity);
+    setShowWindSpeed(response.data.wind.speed);
+    setShowDescription(response.data.weather[0].description);
+    setShowIcon(response.data.weather[0].icon);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+  function replaceCity(event) {
+    setCity(event.target.value);
+  }
   return (
     <div className="App">
       {" "}
       <div className="container">
         <div className="box">
-          <Form />
+          <form id="form" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Enter a city"
+              id="city"
+              onChange={replaceCity}
+            />{" "}
+            <input type="submit" class="btn btn-primary" value="Search" />
+          </form>
+          <button type="button" class="btn btn-warning" id="current">
+            Current
+          </button>
           <br />
           <div class="container text-center">
             <div class="row">
               <div class="col-9 left-heading">
-                <h1></h1>
+                <h1>{showCity}</h1>
                 <h2>
                   <span id="current-day">Sunday </span>,
                   <span id="current-time">17:20</span>
                 </h2>
                 <div class="current-weather">
-                  <img src="/img/almost-cloudy.png" alt="almost cloudy" />
+                  <img
+                    src={`https://openweathermap.org/img/wn/${showIcon}@2x.png`}
+                    alt="almost cloudy"
+                  />
                   <p class="current-weather" id="description">
-                    Windy with periodic clouds
+                    {showDescription}
                   </p>
                 </div>
               </div>
               <div class="col-3 temperature">
                 <div class="current-temperature">
-                  <span id="the-degree">12°</span>
-                  <div class="max-min">
-                    <span id="max">11°</span> /<span id="min">3°</span>
+                  <span id="the-degree">{Math.round(showTemp)}°</span>
+                  <div id="windspeed">
+                    Windspeed: {Math.round(showWindSpeed)} km/h
                   </div>
+                  <div id="humidity">Humidity: {showHumidity}%</div>
                 </div>
               </div>
             </div>
