322c52015ff18eb60ca14ae3cf9715bf095837f4
Zeynab <Nature4explorer@gmail.com>
2023-04-26 05:17:56 +0430

    Installed axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import Form from "./Form";
-import Main from "./Main";
-import Forecast from "./Forecast";
-import Source from "./Source";
+
 export default function App() {
   return (
     <div className="App">
@@ -11,11 +9,79 @@ export default function App() {
         <div className="box">
           <Form />
           <br />
-          <Main />
+          <div class="container text-center">
+            <div class="row">
+              <div class="col-9 left-heading">
+                <h1></h1>
+                <h2>
+                  <span id="current-day">Sunday </span>,
+                  <span id="current-time">17:20</span>
+                </h2>
+                <div class="current-weather">
+                  <img src="/img/almost-cloudy.png" alt="almost cloudy" />
+                  <p class="current-weather" id="description">
+                    Windy with periodic clouds
+                  </p>
+                </div>
+              </div>
+              <div class="col-3 temperature">
+                <div class="current-temperature">
+                  <span id="the-degree">12°</span>
+                  <div class="max-min">
+                    <span id="max">11°</span> /<span id="min">3°</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>{" "}
           <br />
-          <Forecast />
+          <p class="days">Days</p>
+          <hr />
+          <div class="container text-center">
+            <div class="row week">
+              <div class="col">Sat</div>
+              <div class="col">Sun</div>
+              <div class="col">Mon</div>
+              <div class="col">Tue</div>
+              <div class="col">Wed</div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <img src="/img/cloudy.png" alt="cloudy" />
+              </div>
+              <div class="col">
+                <img src="/img/cloudy.png" alt="cloudy" />
+              </div>
+              <div class="col">
+                <img src="/img/sunny.png" alt="Sunny" />
+              </div>
+              <div class="col">
+                <img src="/img/cloudy.png" alt="cloudy" />
+              </div>
+              <div class="col">
+                <img src="/img/cloudy.png" alt="cloudy" />
+              </div>
+            </div>
+            <div class="row numbers">
+              <div class="col">11° / 3°</div>
+              <div class="col">11° / 2°</div>
+              <div class="col">12° / 2°</div>
+              <div class="col">11° / 2°</div>
+              <div class="col">8° / 1°</div>
+            </div>
+          </div>{" "}
         </div>
-        <Source />
+        <p className="below">
+          <a
+            href="https://github.com/ZeynabSherafat/my-weather-app"
+            target="_blank"
+            rel="noreferrer"
+            id="git"
+          >
+            Open-source code
+          </a>{" "}
+          by Zeynab Sherafat
+        </p>{" "}
       </div>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Form.css";
 
 export default function Form() {
+  let [city, setCity] = useState("");
+  function showTemperature(response) {
+    console.log(response);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    axios.get(url).then(showTemperature);
+  }
+  function replaceCity(event) {
+    setCity(event.target.value);
+  }
   return (
     <div className="Form">
-      <form id="form">
+      <form id="form" onSubmit={handleSubmit}>
         <input
           type="text"
           class="form-control"
-          placeholder="Enter the city"
+          placeholder="Enter a city"
           id="city"
+          onChange={replaceCity}
         />{" "}
         <input type="submit" class="btn btn-primary" value="Search" />
       </form>
